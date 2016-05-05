b5b7676767b4797e6ddf104c20ab8f0185350b83
Dane Bratz <danebratz@gmail.com>
2016-05-05 00:42:27 -0600

    changed app name in Gruntfile to match what I've been using; built dist again

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -371,7 +371,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'clientApp',
+          module: 'SkillitAdminApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
