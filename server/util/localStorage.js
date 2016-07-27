22947ab994ae57385524879420e11147f3011cf6
Dane Bratz <danebratz@gmail.com>
2016-07-27 14:27:08 -0600

    bug fix in test

--- server/util/localStorage.js
+++ server/util/localStorage.js
@@ -3,7 +3,7 @@ var redis = require('redis');
 var client = redis.createClient();
 
 client.set("test", "this is poop");
-client.get("test", function(reply, err) {
+client.get("test", function(err, reply) {
   console.log(reply.toString());
 });
 
