b801d10eeffa11307e39cda81ea72bad30f95a17
Dane Bratz <danebratz@gmail.com>
2016-09-17 16:41:36 -0600

    rearranged get methods for articles route

--- server/router/routes/articles.js
+++ server/router/routes/articles.js
@@ -25,6 +25,26 @@ router.get('/', function(req, res, next) {
   }
 });
 
+router.get('/getArticlesTitleId', function(req, res, next) {
+  logger.info('START GET api/articles/getArticlesTitleId');
+  try {
+    Article.model.find({}, '_id title', function(err, articles) {
+      if(err) {
+        logger.error('GET api/articles/getArticlesTitleId', {error: err});
+        return next(err);
+      }
+      var retVal = {
+        data: articles
+      };
+      logger.info('END GET api/articles/getArticlesTitleId');
+      res.json(retVal);
+    });
+  } catch (error) {
+    logger.error('ERROR - exception in GET api/articles/getArticlesTitleId', {error: error});
+    return next(error);
+  }
+});
+
 router.get('/:id', function(req, res, next) {
   try {
     logger.info('START GET api/articles/' + req.params.id);
@@ -90,26 +110,6 @@ router.delete('/:id', function(req, res, next) {
     });
   } catch(error) {
     logger.error('ERROR - exception in DELETE api/articles/:id', {error: error});
-    return next(error);
-  }
-});
-
-router.get('/getArticlesTitleId', function(req, res, next) {
-  logger.info('START GET api/articles/getArticlesTitleId');
-  try {
-    Article.model.find({}, '_id title', function(err, articles) {
-      if(err) {
-        logger.error('GET api/articles/getArticlesTitleId', {error: error});
-        return next(error);
-      }
-      var retVal = {
-        data: articles
-      };
-      logger.info('END GET api/articles/getArticlesTitleId');
-      res.json(retVal);
-    });
-  } catch (error) {
-    logger.error('ERROR - exception in GET api/articles/getArticlesTitleId', {error: error});
     return next(error);
   }
 });
