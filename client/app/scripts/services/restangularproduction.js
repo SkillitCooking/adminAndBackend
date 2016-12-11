7204d0d877cfa714b87a2dcda5296176af89f77e
Dane Bratz <danebratz@gmail.com>
2016-12-10 18:51:15 -0800

    dafd

--- client/app/scripts/services/restangularproduction.js
+++ client/app/scripts/services/restangularproduction.js
@@ -8,11 +8,11 @@
  * Factory in the SkillitAdminApp.
  */
 angular.module('SkillitAdminApp')
-  .factory('RestangularProductionService', function (Restangular, API_PASSWORDS) {
+  .factory('RestangularProductionService', function (Restangular) {
     
     return Restangular.withConfig(function(RestangularConfigurer) {
       RestangularConfigurer.setBaseUrl('https://skillicookingprodapi.info/api/');
-      RestangularConfigurer.setDefaultHeaders({password: API_PASSWORDS.PROD});
+      RestangularConfigurer.setDefaultHeaders({password: "sm@34MLPG9L&rWph|YMwcg=&5|R3TMZ!!H+F48ThGFl56E&*RD"});
     });
 
   });
