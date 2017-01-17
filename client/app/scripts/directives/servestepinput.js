70c8e517796ce3779a7d6e710e0fb791548c6bbe
Dane Bratz <danebratz@gmail.com>
2017-01-16 16:40:55 -0800

    something

---
+++ client/app/scripts/directives/servestepinput.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name SkillitAdminApp.directive:customStepInput
+ * @description
+ * # serveStepInput
+ */
+angular.module('SkillitAdminApp')
+  .directive('serveStepInput', function () {
+    return {
+      templateUrl: 'views/servestepinput.html',
+      restrict: 'E',
+      scope: true,
+      transclude: false,
+      link: function (scope, element, attrs) {
+        scope.integerval = /^\d*$/;
+        scope.tipAdded = false;
+        scope.tipIsMinimized = true;
+
+        if(scope.constructingStep.stepId) {
+          //then step already exists, need to load stepSpecifics, productName
+          scope.constructingStep.productName = scope.constructingStep.productKeys[0];
+          if(scope.constructingStep.stepTip) {
+            //then existing tip
+            scope.tipAdded = true;
+          }
+        } else {
+          //then new step, needs appropriate initialization
+          scope.constructingStep.stepSpecifics = [{
+            propName: "serveStepText",
+            val: ""
+          }];
+        }
+
+        scope.toggleTipVisibility = function() {
+          scope.tipIsMinimized = !scope.tipIsMinimized;
+        };
+
+        scope.getTipToggleText = function() {
+          if(scope.tipIsMinimized) {
+            return 'Expand';
+          } else {
+            return 'Minimize';
+          }
+        };
+
+        scope.addTip = function() {
+          scope.tipAdded = !scope.tipAdded;
+          scope.tipIsMinimized = false;
+          scope.stepTip = {
+            videoInfo: {}
+          };
+        };
+
+        scope.removeTip = function() {
+          scope.tipAdded = !scope.tipAdded;
+          scope.constructingStep.stepTip = undefined;
+        };
+      }
+    };
+  });
